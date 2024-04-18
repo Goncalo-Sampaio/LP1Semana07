83a679dfa5aeac87c11e0eb0d7c0987d1b537333
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-04-18 09:39:08 +0100

    Changed Program to instantiate both classes and test them, GameUnits project

--- GameUnits/Program.cs
+++ GameUnits/Program.cs
@@ -6,7 +6,13 @@ namespace GameUnits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MilitaryUnit mU = new MilitaryUnit(1, 5, 2);
+            SettlerUnit sU = new SettlerUnit();
+
+            mU.Move(2);
+            sU.Move(1);
+            Console.WriteLine($"Military health: {mU.Health}, cost: {mU.Cost}");
+            Console.WriteLine($"Settler health: {sU.Health}, cost: {sU.Cost}");
         }
     }
 }

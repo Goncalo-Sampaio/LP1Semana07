03ffaa8adc57c7b72c6792b737aaf81d929d3e52
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-04-18 09:08:50 +0100

    Unit class is now abstract, added move method and abstract Cost property

--- GameUnits/Unit.cs
+++ GameUnits/Unit.cs
@@ -1,14 +1,22 @@
+using System;
+
 namespace GameUnits
 {
-    public class Unit
+    public abstract class Unit
     {
         private int movement;
         public int Health { get; set; }
+        public abstract float Cost { get; }
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
+        }
+
+        public void Move(int move){
+            this.movement += move;
+            Console.WriteLine(move);
         }
     }
 }
